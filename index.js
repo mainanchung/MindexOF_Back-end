43413071c827b65fab33614b674539e6aa5dbc47
mainanchung <melodychung522@gmail.com>
2022-12-07 02:56:54 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 8080;
+
+app.use(cors());
+app.use(express.json())
+app.use(express.static('public'));
